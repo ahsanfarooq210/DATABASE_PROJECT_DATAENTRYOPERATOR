2c8573834fcd950654ae5c8518054b140bf2c4dc
ahsanfarooq210 <ahsanfarooq210@gmail.com>
2020-02-29 10:51:40 +0500

    Initial commit

---
+++ app/src/main/java/com/example/database_project_dataentryoperator/Main_dashboard_activity.java
@@ -0,0 +1,16 @@
+package com.example.database_project_dataentryoperator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main_dashboard_activity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_dashboard_activity);
+    }
+}
