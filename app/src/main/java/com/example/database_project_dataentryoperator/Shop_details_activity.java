690f8c504867c346f619b28a8cc827f5cc2131dd
ahsanfarooq210 <ahsanfarooq210@gmail.com>
2020-03-30 02:14:42 +0500

    dashboard

--- app/src/main/java/com/example/database_project_dataentryoperator/Shop_details_activity.java
+++ app/src/main/java/com/example/database_project_dataentryoperator/Shop_details_activity.java
@@ -2,7 +2,11 @@ package com.example.database_project_dataentryoperator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
 
 public class Shop_details_activity extends AppCompatActivity
 {
@@ -12,5 +16,8 @@ public class Shop_details_activity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_details_activity);
+        LatLng objLatLng=getIntent().getExtras().getParcelable("latlan");
+
+        Toast.makeText(this, "latitude= "+objLatLng.latitude+"  longitude= "+objLatLng.longitude, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/database_project_dataentryoperator/add_shops_activity.java
+++ app/src/main/java/com/example/database_project_dataentryoperator/add_shops_activity.java
@@ -1,8 +1,12 @@
 package com.example.database_project_dataentryoperator;
 
+import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.fragment.app.FragmentActivity;
@@ -24,15 +28,30 @@ public class add_shops_activity  extends FragmentActivity implements OnMapReadyC
     private GoogleMap mMap;
     private Geocoder geo;
     private List<Address> address;
+    private LatLng coordinates;
+    private Button cnfrml;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_shops_activity);
+        cnfrml=findViewById(R.id.cnfrm_coordinates);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        cnfrml.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent=new Intent(add_shops_activity.this,Shop_details_activity.class);
+                intent.putExtra("latlan",coordinates);
+
+                startActivity(intent);
+
+            }
+        });
     }
 
     @Override
@@ -45,6 +64,7 @@ public class add_shops_activity  extends FragmentActivity implements OnMapReadyC
             mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
                 @Override
                 public void onMapClick(LatLng latLng) {
+                    coordinates=latLng;
                     try {
 
                             if (geo == null)
@@ -57,7 +77,7 @@ public class add_shops_activity  extends FragmentActivity implements OnMapReadyC
                                 mMap.addMarker(new MarkerOptions().position(latLng).title("Name:" + address.get(0).getCountryName()
                                         + ". Address:" + address.get(0).getAddressLine(0)));
                                 Toast.makeText(add_shops_activity.this, "Name:" + address.get(0).getCountryName() + ". Address:" + address.get(0).getAddressLine(0), Toast.LENGTH_SHORT).show();
-
+                                 cnfrml.setVisibility(View.VISIBLE);
                         } catch (IOException ex) {
                             if (ex != null)
                                 Toast.makeText(add_shops_activity.this, "Error:" + ex.getMessage().toString(), Toast.LENGTH_LONG).show();
