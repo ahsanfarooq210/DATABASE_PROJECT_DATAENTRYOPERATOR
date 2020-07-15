34ecf96c810d24c7c0f43188d21fc4bd3b1aaf73
Hazeem Hassan <hhazimhassan@gmail.com>
2020-07-15 16:11:55 +0500

    login

---
+++ app/src/main/java/com/example/database_project_dataentryoperator/ProfileActivities/setting_activity.java
@@ -0,0 +1,16 @@
+package com.example.database_project_dataentryoperator.ProfileActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.database_project_dataentryoperator.R;
+
+public class setting_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
